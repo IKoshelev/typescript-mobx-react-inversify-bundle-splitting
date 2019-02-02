f8abc3a56a7eea5892fd6f6e252ae63cbf01d93b
IKoshelev <koshelew@live.com>
2019-02-02 21:52:37 +0100

    Added showcase of including your own highly-reused files into vendors bundle.

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,25 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.ts', '.tsx']
   },
+  optimization:{
+    //More on the configuration on splitting:
+    // https://gist.github.com/sokra/1522d586b8e5c0f5072d7565c2bee693#configurate-cache-groups
+    // https://wanago.io/2018/06/04/code-splitting-with-splitchunksplugin-in-webpack-4/
+    splitChunks: {
+      cacheGroups: {
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+        /*change the name prop name to change bundle name*/
+        vendors: {
+          test: /[\\/]node_modules[\\/]|HttpClient.*$|ServiceBase.*$|IoC*.*$/,
+          priority: -10
+        }
+      }
+    }
+  },
   module: {
     rules: [{
       test: /\.tsx?$/,
