4a6f5e7db49cf1157d00f55c6559a942da2b8c57
IKoshelev <koshelew@live.com>
2019-02-03 20:25:12 +0100

    Added example of having a type configured as singleton for injection.

--- src/IoC/IoC-public.ts
+++ src/IoC/IoC-public.ts
@@ -8,8 +8,13 @@ export function bindToSelf(target: Function){
     defaultContainer.bind(target).toSelf();
 }
 
+export function bindToSelfSingleton(target: Function){
+    inversify.injectable()(target);
+    defaultContainer.bind(target).toSelf().inSingletonScope();
+}
+
 export function getIocFactory<T>(target: inversify.interfaces.Newable<T>): () => T {
-    return () => defaultContainer.resolve(target);
+    return () => defaultContainer.get(target);
 }
 
 export * from 'inversify';
--- src/services/HttpClient.ts
+++ src/services/HttpClient.ts
@@ -2,8 +2,13 @@ import * as ioc from './../IoC/IoC-public';
 
 console.log("executing HttpClient.ts");
 
-@ioc.bindToSelf
+@ioc.bindToSelfSingleton
 export class HttpClient {
+
+    public constructor(){
+        console.log("Constructing instance of HttpClient. This message should only appear once ever.");
+    }
+
     public doRequest(){
 
     }
